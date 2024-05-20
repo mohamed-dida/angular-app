89b15cb1168edf9f13eea066b909897c182b3c36
dida <elsammancompany3@gmail.com>
2024-05-20 09:44:39 +0200

    initial commit

---
+++ src/app/protoflio/protoflio.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-protoflio',
+  templateUrl: './protoflio.component.html',
+  styleUrls: ['./protoflio.component.css']
+})
+export class ProtoflioComponent {
+
+}
